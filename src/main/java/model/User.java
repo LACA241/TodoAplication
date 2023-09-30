83cc77ffa97f4e7ac261cfc78e0bd59f4bfabc67
ladislav <ladislavszatmaei@gmail,com>
2023-10-01 01:14:51 +0200

    Main applikation a entyty user1

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,6 +1,9 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -13,5 +16,64 @@ public class User {
     @Column(name = "first_name")
     protected String first_name;
 
+    @Column(name = "surename")
+    protected String surename;
 
+    @Column(name = "phone")
+    protected Long phone;
+
+    @Column(name = "email_address")
+    protected String email_address;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.EAGER)
+    @JsonIgnore
+    protected Set<Task>tasks;
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) {
+        this.surename = surename;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
 }
