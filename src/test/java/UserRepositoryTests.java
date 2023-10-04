39ab096d1116a3d1612aea21c85d494b865e6058
ladislav <ladislavszatmaei@gmail,com>
2023-10-04 22:17:29 +0200

    UserRepository Test

--- src/test/java/UserRepositoryTests.java
+++ src/test/java/UserRepositoryTests.java
@@ -6,20 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class UserRepositoryTests {
-@Autowired
+    @Autowired
     UserRepositories repositories;
-@Test
-    void testLoadUsers(){
-    List<User>userList=repositories.findAll();
-assertThat(userList).isNotEmpty();
-}
+
+    @Test
+    void testLoadUsers() {
+        List<User> userList = repositories.findAll();
+        assertThat(userList).isNotEmpty();
+    }
+
+
 }
