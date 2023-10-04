a86efb45b0b6be9a2f3962b34b7378de0d7df143
ladislav <ladislavszatmaei@gmail,com>
2023-10-04 17:38:58 +0200

    entyty user a task

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,6 +1,104 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "Task")
 public class Task {
 
+    @Column(name = "id_task")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
 
+    @Column(name = "title")
+    protected String title;
+
+    @Column(name = "description")
+    protected String description;
+
+    @Column(name = "status")
+    protected String status;
+
+    @Column(name = "due_date")
+    protected Date due_date;
+
+    @Column(name = "updated_date")
+    protected Date updated_date;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    protected User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @OneToMany(mappedBy = "task",fetch = FetchType.EAGER)
+@JsonIgnore
+protected Set<Task>tasks;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDue_date() {
+        return due_date;
+    }
+
+    public void setDue_date(Date due_date) {
+        this.due_date = due_date;
+    }
+
+    public Date getUpdated_date() {
+        return updated_date;
+    }
+
+    public void setUpdated_date(Date updated_date) {
+        this.updated_date = updated_date;
+    }
 }
