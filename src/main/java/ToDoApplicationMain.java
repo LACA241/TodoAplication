fa9f5d885f4fbc8fbdbc514d1d9722499271adfb
ladislav <ladislavszatmaei@gmail,com>
2023-10-04 18:59:27 +0200

    testovacie data1

--- src/main/java/ToDoApplicationMain.java
+++ src/main/java/ToDoApplicationMain.java
@@ -1,7 +1,13 @@
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@Slf4j
+@EnableScheduling
 public class ToDoApplicationMain {
     public static void main(String[] args) {
         SpringApplication.run(ToDoApplicationMain.class, args);
