89be34364e4b43a363c48bf8a130c5937e5d1ef3
ladislav <ladislavszatmaei@gmail,com>
2023-10-06 17:45:11 +0200

    1

--- src/main/java/ToDoAplication/Repositories/TaskRepositories.java
+++ src/main/java/ToDoAplication/Repositories/TaskRepositories.java
@@ -1,4 +1,13 @@
 package ToDoAplication.Repositories;
 
-public interface TaskRepositories {
+import ToDoAplication.model.Task;
+import ToDoAplication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RestResource(path = "tasks")
+public interface TaskRepositories extends JpaRepository<User, Long> {
 }
--- src/main/java/ToDoAplication/Repositories/UserRepositories.java
+++ src/main/java/ToDoAplication/Repositories/UserRepositories.java
@@ -10,10 +10,9 @@ import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
-
 public interface UserRepositories extends JpaRepository<User,Long> {
-List<User> findAllByFirst_nameOrSurename(String first_name,String surename);
+List<User> findAllByFirstnameOrSurename(String first_name,String surename);
 @Query
-List<User>findAllByAnyFirst_name(@Param("searchString") String search, Pageable page);
+List<User>findAllByAnyFirstname(@Param("searchString") String search, Pageable page);
 
 }
